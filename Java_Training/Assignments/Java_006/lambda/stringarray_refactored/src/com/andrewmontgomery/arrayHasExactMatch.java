8460c080b29e6da349854f78ef631ea0f75e40b9
Your Name <wamontgomery0068@gmail.com>
2019-07-17 06:50:28 -0500

    Completed

---
+++ Java_Training/Assignments/Java_006/lambda/stringarray_refactored/src/com/andrewmontgomery/arrayHasExactMatch.java
@@ -0,0 +1,5 @@
+package com.andrewmontgomery;
+
+public interface arrayHasExactMatch {
+    boolean arrayHasExactMatchs(String []arrayString, String string, boolean yesOrNo);
+}
---
+++ Java_Training/Assignments/Java_006/lambda/stringarray_refactored/src/com/andrewmontgomery/indexOfOccuranceInArray.java
@@ -0,0 +1,5 @@
+package com.andrewmontgomery;
+
+public interface indexOfOccuranceInArray {
+    int[] indexOfOccuranceInArrays(String[] array, String string, boolean maybe);
+}
